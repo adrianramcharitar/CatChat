161293ca747d6e45dbca2e4b62948866269b7f39
Adrian Ramcharitar <adrian_ramcharitar@trendmicro.com>
2018-08-01 14:27:08 -0400

    implementing variable names

--- src/components/UserPanel/UserForm/InputForm/InputForm.js
+++ src/components/UserPanel/UserForm/InputForm/InputForm.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
 import './InputForm.css';
+import PropTypes from 'prop-types';
 
-class InputForm extends Component{
+class InputForm extends Component {
     render() {
+        const { title, value, placeholderText, buttonText } = this.props;
         return (
             <form className="InputForm">
-                <header>Join Chat</header>
+                <header>{title}</header>
                 <fieldset className="button-input">
-                    <input type="text" value="Test" />
-                    <button>Join</button>
+                    <input type="text" value={value} placeholder={placeholderText} />
+                    <button>{buttonText}</button>
                 </fieldset>
             </form>
         );
     }
 }
+
+InputForm.propTypes = {
+    title: PropTypes.string,
+    value: PropTypes.string,
+    placeholderText: PropTypes.string,
+    buttonText: PropTypes.string,
+};
 
 export default InputForm;
